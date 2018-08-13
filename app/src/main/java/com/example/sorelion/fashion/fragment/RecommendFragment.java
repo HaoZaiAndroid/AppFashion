b288cbdd1f1a2df4c452394621de80f6b2ad85bb
HaoZaiAndroid <yh_hzhx@163.com>
2018-08-13 22:13:24 +0800

    实现网易云音乐 tab切换特效

--- app/src/main/java/com/example/sorelion/fashion/fragment/RecommendFragment.java
+++ app/src/main/java/com/example/sorelion/fashion/fragment/RecommendFragment.java
@@ -1,9 +1,9 @@
 package com.example.sorelion.fashion.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
@@ -12,6 +12,8 @@ import com.example.sorelion.fashion.R;
 import com.example.sorelion.fashion.adapter.RecommendPagerAdapter;
 import com.example.sorelion.fashion.base.BaseFragment;
 import com.example.sorelion.fashion.databinding.RecommendFragmentBinding;
+import com.example.sorelion.fashion.utils.Util;
+import com.example.sorelion.fashion.widget.tablayout.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,7 @@ public class RecommendFragment extends BaseFragment<RecommendFragmentBinding> {
 
     private List<String> mTitleList = new ArrayList<>();
     private List<Fragment> mFragments = new ArrayList<>();
+    private Context context;
 
     @Override
     public int setContentLayout() {
@@ -35,6 +38,7 @@ public class RecommendFragment extends BaseFragment<RecommendFragmentBinding> {
     }
 
     private void init() {
+        context = getContext();
         initID();
         initFragment();
 
@@ -46,9 +50,13 @@ public class RecommendFragment extends BaseFragment<RecommendFragmentBinding> {
         //mTitleList.add(getString(R.string.recommend_tab_three));
         mFragments.add(new ChoicenessFragment());
         mFragments.add(new FriendFragment());
-        RecommendPagerAdapter adapter = new RecommendPagerAdapter(getChildFragmentManager(),mTitleList,mFragments);
+        RecommendPagerAdapter adapter = new RecommendPagerAdapter(getChildFragmentManager(), mTitleList, mFragments);
         viewPager.setAdapter(adapter);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setNeedSwitchAnimation(true);
+
+        tabLayout.setSelectedTabIndicatorWidth(Util.Dp2px(context, 25));
+        //tabLayout.setIndicatorWidthWrapContent(true)
         tabLayout.setupWithViewPager(viewPager);
     }
 
